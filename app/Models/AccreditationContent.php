3471be387c0ea4531f29c7c548e384479a0c970c
Ibnu Akil <ibnu.ial@bsi.ac.id>
2024-10-07 18:27:09 +0700

    update rekapitulasi

---
+++ app/Models/AccreditationContent.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class AccreditationContent extends Model
+{
+    use HasFactory;
+    use HasUuids;
+    public $timestamps = false;
+    protected $table = 'accreditation_contents';
+    protected $fillable = [
+        'aspect',
+        'statement',
+        'value',
+        'type',
+        'aspectable_id',
+        'main_component_id',
+        'instrument_aspect_point_id',
+        'accreditation_id'        
+    ];
+    
+}
