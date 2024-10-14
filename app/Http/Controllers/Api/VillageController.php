9687b0f6e946a0e6325ea5c26ee60667d2268a0d
Ibnu Akil <ibnu.ial@bsi.ac.id>
2024-10-14 08:43:27 +0700

    update rekapitulasi

--- app/Http/Controllers/Api/VillageController.php
+++ app/Http/Controllers/Api/VillageController.php
@@ -44,7 +44,7 @@ class VillageController extends BaseController
 		return $this->sendResponse($response, "Success", $total);		
 	}
 
-    public function getBySubdistrictId(Request $request, $subdistrict_id)//with filter
+    public function getBySubdistrict(Request $request, $subdistrict_id)//with filter
 	{
 		$query = Village::query();		
         $query->where('subdistrict_id', '=', $subdistrict_id);			
