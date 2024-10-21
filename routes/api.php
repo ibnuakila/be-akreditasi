8b15a5cbab4ab6afd272447dcff97e3cc9e85234
Ibnu Akil <ibnu.ial@bsi.ac.id>
2024-10-21 11:33:36 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -27,25 +27,25 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/institution/index/', [InstitutionController::class, 'index']);
     Route::post('/institution/store', [InstitutionController::class, 'store']);
     Route::get('/institution/show/{id}', [InstitutionController::class, 'show']);
     Route::put('/institution/update/{model}', [InstitutionController::class, 'update']);
     Route::delete('/institution/destroy/{model}', [InstitutionController::class, 'destroy']);
     Route::get('/institution/list', [InstitutionController::class, 'list']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/province/index/', [ProvinceController::class, 'index']);
     Route::post('/province/store', [ProvinceController::class, 'store']);
     Route::get('/province/show/{id}', [ProvinceController::class, 'show']);
     Route::put('/province/update/{model}', [ProvinceController::class, 'update']);
     Route::delete('/province/destroy/{model}', [ProvinceController::class, 'destroy']);
     Route::get('/province/list', [ProvinceController::class, 'list']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/city/index/', [CityController::class, 'index']);
     Route::post('/city/store', [CityController::class, 'store']);
     Route::get('/city/show/{id}', [CityController::class, 'show']);
@@ -53,9 +53,9 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::delete('/city/destroy/{model}', [CityController::class, 'destroy']);
     Route::get('/city/list', [CityController::class, 'list']);
     Route::get('/city/getbyprovince/{province_id}', [CityController::class, 'getByProvince']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/subdistrict/index/', [SubdistrictController::class, 'index']);
     Route::post('/subdistrict/store', [SubdistrictController::class, 'store']);
     Route::get('/subdistrict/show/{id}', [SubdistrictController::class, 'show']);
@@ -63,9 +63,9 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::delete('/subdistrict/destroy/{model}', [SubdistrictController::class, 'destroy']);
     Route::get('/subdistrict/list', [SubdistrictController::class, 'list']);
     Route::get('/subdistrict/getbycity/{city_id}', [SubdistrictController::class, 'getByCity']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/village/index/', [VillageController::class, 'index']);
     Route::post('/village/store', [VillageController::class, 'store']);
     Route::get('/village/show/{id}', [VillageController::class, 'show']);
@@ -73,27 +73,27 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::delete('/village/destroy/{model}', [VillageController::class, 'destroy']);
     Route::get('/village/list', [VillageController::class, 'list']);
     Route::get('/village/getbysubdistrict/{subdistrict_id}', [VillageController::class, 'getbysubdistrict']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/region/index/', [RegionController::class, 'index']);
     Route::post('/region/store', [RegionController::class, 'store']);
     Route::get('/region/show/{id}', [RegionController::class, 'show']);
     Route::put('/region/update/{model}', [RegionController::class, 'update']);
     Route::delete('/region/destroy/{model}', [RegionController::class, 'destroy']);
     Route::get('/region/list', [RegionController::class, 'list']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/proposaldocument/index/', [ProposalDocumentController::class, 'index']);
     Route::post('/proposaldocument/store', [ProposalDocumentController::class, 'store']);
     Route::get('/proposaldocument/show/{id}', [ProposalDocumentController::class, 'show']);
     Route::put('/proposaldocument/update/{model}', [ProposalDocumentController::class, 'update']);
     Route::delete('/proposaldocument/destroy/{model}', [ProposalDocumentController::class, 'destroy']);
     Route::get('/proposaldocument/list', [ProposalDocumentController::class, 'list']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/proposal/index/', [ProposalController::class, 'index']);
     Route::get('/proposal/list/', [ProposalController::class, 'list']);
     Route::post('/proposal/store', [ProposalController::class, 'store']);
@@ -101,23 +101,23 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/proposal/show/{id}', [ProposalController::class, 'show']);
     Route::put('/proposal/update/{model}', [ProposalController::class, 'update']);
     Route::delete('/proposal/destroy/{model}', [ProposalController::class, 'destroy']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/instrument/get-instrument/{params}', [InstrumentController::class, 'getInstrument']);
     Route::post('/instrument/generate-proposal-document/', [InstrumentController::class, 'generateProposalDocument']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/evaluationassignment/index/', [EvaluationAssignment::class, 'index']);
     Route::get('/evaluationassignment/list/', [EvaluationAssignment::class, 'list']);
     Route::get('/evaluationassignment/show/{id}', [EvaluationAssignment::class, 'show']);
     Route::post('/evaluationassignment/store/', [EvaluationAssignment::class, 'store']);
     Route::post('/evaluationassignment/update/', [EvaluationAssignment::class, 'update']);
     Route::delete('/evaluationassignment/destroy/{id}', [EvaluationAssignment::class, 'destroy']);
-});
+//});
 
-Route::middleware('auth:sanctum')->group(function(){
+//Route::middleware('auth:sanctum')->group(function(){
     Route::get('/accreditation/index/{user_id}', [AccreditationController::class, 'index']);
     Route::post('/accreditation/store', [AccreditationController::class, 'store']);
     Route::get('/accreditation/show/{id}', [AccreditationController::class, 'show']);
@@ -127,4 +127,4 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::get('/accreditation/addnew/{user_id}', [AccreditationController::class, 'addNew']);
     Route::post('/accreditation/store-files', [AccreditationController::class, 'storeFiles']);
     Route::get('/accreditation/edit/{user_id}', [AccreditationController::class, 'addNew']);
-});
+//});
