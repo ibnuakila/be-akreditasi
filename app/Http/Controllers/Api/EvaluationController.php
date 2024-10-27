8c99dba5d0457478e2d52e0ba1355a638f369622
Ibnu Akil <ibnu.ial@bsi.ac.id>
2024-10-27 09:09:27 +0700

    add cors dan evaluationcontroller

---
+++ app/Http/Controllers/Api/EvaluationController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class EvaluationController extends BaseController implements ICrud
+{
+    //
+    /**
+	 * 
+	 * @param $model
+	 */
+	public function destroy($id){
+
+    }
+
+	public function index(){
+        
+    }
+
+	/**
+	 * 
+	 * @param $id
+	 */
+	public function show($id){
+
+    }
+
+	/**
+	 * 
+	 * @param $request
+	 */
+	public function store(Request $request){
+
+    }
+
+	/**
+	 * 
+	 * @param $request
+	 * @param $model
+	 */
+	public function update(Request $request, $id){
+
+    }
+}
